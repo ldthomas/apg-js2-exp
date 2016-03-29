7849ab15c4cee4f3ce1cd5f58e434a6ab15012e5
Lowell D. Thomas <lowell@coasttocoastresearch.com>
2016-03-29 09:54:52 -0400

    Added src/apgexpjs-gen.js and src/apgexpcss-gen.js for generating apgexp.js and apgexp.css. Updated apgexp.js and apgexp.css. Updated documentation.

--- css-gen.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  var fs = require("fs");
-  var apglib = require("apg-lib");
-  var css = apglib.utils.css();
-  fs.writeFileSync("apgexp.css", css);
-})()
--- src/apgexpjs-gen.js
+++ src/apgexpjs-gen.js
@@ -1,18 +1,17 @@
-// This function is used to generate a browser-accessible copy of `apg-exp.js`.
+// This function is used to generate a browser-accessible copy of `apg-exp`.
 // To generate and minify:
 // ```
-//npm install -g browserify
-//npm install -g uglifyjs
-//browserify browser.js > apgexp.js
-// uglifyjs apgexp.js --compress --mangle >apgexp-min.js
+// npm install -g browserify
+// npm install -g uglifyjs
+// browserify apgexpjs-gen.js | uglifyjs --compress --mangle > apgexp.js
 // ```
-// To use it in a browser, include apgexp.js or apgexp-min.js in a script in the web page header.
+// To use it in a browser, include apgexp.js 
+// and the style sheet, apgexp.css, in a script in the web page header.
 // ```
-//<!DOCTYPE html>
-//<html lang="en">
 //<head>
 // ...
-//<script src="apgexp-min.js" charset="utf-8"></script>
+// <link rel="stylesheet" href="apgexp.css">
+// <script src="apgexp.js" charset="utf-8"></script>
 // ...
 //</head>
 // ```
@@ -25,13 +24,17 @@
 //  var exec = function(){
 //    var grammar = 'rule = "abc"\n';
 //    var str = "---abc---";
-//    /* use apg-exp */
+//    /* 
+//     * use apg-exp
+//    */
 //    var exp = new apgexp(grammar);
 //    var result = exp.exec(str);
 //    ... /* do something with result */
-//    /* use an apg-lib utilities function */
+//    /*
+//     * use an apg-lib utilities function
+//    */
 //    var strHtml = apglib.utils.stringToAsciiHtml(str);
-//    ... /* do something with the HTML version of the string */
+//    ... /* do something with strHtml */
 //  }
 //  </script>
 // ```
